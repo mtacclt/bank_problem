0c52b4596339905e350c1b1b70b2658b5b119612
ACCOLITE\marek.tomecek <marek.tomecek@accolitedigital.com>
2022-11-14 18:38:57 +0000

    create testing for customer/system interaction

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,19 @@
 import bank.pkg.SantanderBank;
+import bank.pkg.SantanderCustomer;
 import bank.pkg.SantanderStorage;
+import bank.pkg.SantanderTransactionSystem;
 
 import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
+
+        // testing transactionSystem to Customer interoperability
+        SantanderBank bank = new SantanderBank("Santander","UK");
+        SantanderTransactionSystem transactionSystem = new SantanderTransactionSystem(bank);
+        SantanderCustomer customer = new SantanderCustomer("Marek",23,transactionSystem);
+        customer.deposit(1000);
+        customer.withdraw(1000);
+
     }
 }
